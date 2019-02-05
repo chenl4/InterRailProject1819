ba55e79e6712dfc87704796e38cd5b1b2de58046
nordqunn <nordqunn@rose-hulman.edu>
2019-02-05 12:24:53 -0500

    fixed quotes

--- Map.java
+++ Map.java
@@ -1,7 +1,7 @@
 public class Map{
 	static void main(String[] args){
 		System.out.println("First thing: Hello from Nate!");
-		System.out.println("Second thinnng: mistkae from Ntae);
+		System.out.println("Second thinnng: mistkae from Ntae"); //TODO: fix
 	}
 }
 
